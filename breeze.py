c72dc29e5ed226fd7f63c850e6a536864581cb04
Antelope10 <86484092+Antelope10@users.noreply.github.com>
2022-11-03 10:22:17 -0500

    update

--- breeze.py
+++ breeze.py
@@ -1,1 +1,14 @@
-init
+import hashlib
+
+class Breeze_block:
+    def __init__(self, transactions, previous_hash):
+        self.transactions = transactions
+        self.previous_hash = previous_hash
+
+    def raw_hash(transactions, previous_hash):
+        raw = transactions + "-" + previous_hash
+        return hashlib.sha256(raw)
+
+    def nonce_hash(transactions, previous_hash, zeros_count):
+        return TODO
+
