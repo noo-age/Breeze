8ed176c2b133ab5253ea14d356705661c30ef443
Antelope10 <86484092+Antelope10@users.noreply.github.com>
2022-11-07 11:38:49 -0600

    update

--- breeze.py
+++ breeze.py
@@ -6,37 +6,39 @@ class Breeze_block:
         self.transactions = transactions
         self.previous_hash = previous_hash
         self.difficulty = difficulty
-        raw = str(transactions) + "-" + str(previous_hash)
-        raw_hash = hashlib.sha256(raw)
 
-    def proof_of_work(transactions, previous_hash, difficulty):
+    def proof_of_work(self,transactions,previous_hash,difficulty):
         max_nonce = 2 ** 32
         a = ""
         hash_result = 0
         final_nonce = 0
         for nonce in range(max_nonce):
             hash_result = hashlib.sha256(str(transactions)+"-"+str(previous_hash)+"-"+str(nonce)).hexdigest()
-
             if str(hash_result)[0,difficulty] == a.zfill(difficulty):
                 final_nonce = nonce
                 break
-
         return final_nonce, hash_result
 
-    def generate_block(transactions, previous_hash, difficulty):
-        nonce, hash = proof_of_work(transactions, previous_hash, difficulty)
+    def generate_block(self,transactions, previous_hash, difficulty):
+        nonce, hash = self.proof_of_work(transactions, previous_hash, difficulty)
+
+    def transact():
+        current_coin = input("Enter current coin: ")
+        public_key = input("Enter the public key of the user you wish to transact to: ")
+        private_key = input("Enter your private key: ")
+
 
 
 
 
 def main():
-    print("Type in which action you want to take: \n")
-    print("- Transact\n")
-    print("- Create Block\n")
+    print("Type in the number of the action you want to take: \n")
+    print("1 - Transact\n")
+    print("2 - Create Block\n")
     action = input("Enter: ")
-    if action.lower() == "transact":
+    if action.lower() == "1":
         print("Transaction complete")
-    elif action.lower() == "create block":
+    elif action.lower() == "2":
         print("Block Created")
     else:
         print("Invalid Input")
