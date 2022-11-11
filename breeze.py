92985804fed18df527156d79240b44a27a9de25e
Antelope10 <86484092+Antelope10@users.noreply.github.com>
2022-11-10 20:28:44 -0600

    update

--- breeze.py
+++ breeze.py
@@ -1,6 +1,39 @@
 import hashlib
+import random
+from math import gcd as bltin_gcd
 
-class Breeze_block:
+def coprime(a, b):
+    return bltin_gcd(a, b) == 1
+
+def generate_key_pair():
+    p = random.randrange(1000,2000)
+    q = p+1
+    while 1:
+        if coprime(p,q):
+            break
+        q += 1
+    n = p*q
+    totient_n = n * (1-1/p) * (1-1/q)
+    e = 100
+    while 1:
+        if coprime(n,e):
+            break
+        e += 1
+    d = 0
+    k = 1
+    while 1:
+        d = (k * totient_n+1)/e
+        if int(d) == d:
+            break
+    public_key = str(n) + "-" + str(e)
+    private_key = str(d)
+    print("Public Key: " + public_key)
+    print("Private Key: " + private_key)
+    print("Store key-pairs in a safe, easily accessible location. They are your Breeze identity")
+    
+
+
+class Breeze_block:        
 
     def __init__(self, transactions, previous_hash, difficulty):
         self.transactions = transactions
@@ -21,14 +54,23 @@ class Breeze_block:
 
     def generate_block(self,transactions, previous_hash, difficulty):
         nonce, hash = self.proof_of_work(transactions, previous_hash, difficulty)
-
+        block = str(transactions) + "-" + str(previous_hash)+ "-" + str(nonce) + "-" + str(hash)
+        print(block)
+    
     def transact():
         current_coin = input("Enter current coin: ")
         public_key = input("Enter the public key of the user you wish to transact to: ")
         private_key = input("Enter your private key: ")
 
-        block = str(transactions) + "-" + str(previous_hash)+ "-" + str(nonce) + "-" + str(hash)
-        print(block)
+        message = str(current_coin) + "-" + str(public_key)
+        md = hashlib.sha256(message)
+
+
+
+
+    
+
+        
 
 
 
